88f3aace74f4314f4fe450f2062a9979ab266298
CTorkar <torkar.chris@gmail.com>
2026-03-15 20:53:20 +0100

    Update projects.js

--- projects.js
+++ projects.js
@@ -32,7 +32,7 @@ description:"Portable digital station for FT8 and weak signal modes."
 
 {
 title:"Kenwood TR9000 Repair",
-image:"images/tr9000.jpg",
+image:"images/kenwoodtr9000/1.jpg",
 link:"projects/kenwood_tr9000.html",
 tags:["repair","transceiver"],
 description:"Repair and modification of a classic 2m transceiver."
